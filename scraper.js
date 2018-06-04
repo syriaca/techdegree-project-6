2cdd129432df0236e8418c2bc378f08738df6e30
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-06-04 10:39:58 +0200

    Prepare, plan, perform, perfect

---
+++ scraper.js
@@ -0,0 +1,13 @@
+// Prepare, Plan, Perform, Perfect
+
+// Create an app using Node.js For a comparaison prices sites,
+
+// That's a command line application running once a day
+// Scrape a website,
+// Check a folder called "data"
+    // If the folder doesn't exist, create one
+    // else "do nothing"
+// Populate the latests price
+// Save as a spreadsheet (CSV) 
+// Should use npm moudule to assist me in project (content scraper & create csv file)
+
