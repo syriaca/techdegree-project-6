8e74aa01d48b67a0f0ca451e0d6d25a48de2b504
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-06-06 10:31:32 +0200

    Added local server
    Added a local server by copying / pasting node.js API documentation example + migrate instructions to readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # techdegree-project-6
+// ==== Prepare, Plan, Perform, Perfect ==== //
+
+//== Create a command line application app using Node.js For a comparaison prices sites,
+//== Should use npm moudule to assist me in project (content scraper & create csv file)
+    // At least 1,000 downloads
+    // Has been updated in the last six months
+
+//== Scrape a website once a day
+    
+    //== 1) uses http://shirts4mike.com/shirts.php as single entry point to scrape information for 8 tee-shirts from the site, 
+    // without using any hard-coded urls like http://www.shirts4mike.com/shirt.php?id=101
+
+    //== 2) get price, title, url and image url save this information into a CSV file name formated like: YYYY-MM-DD.csv.
+
+    //== 3) the column headers in the CSV need to be in a certain order to be correctly entered into a database. They should be in this order: Title, Price, ImageURL, URL, and Time
+    
+    //== 4) Save the file ine a folde called "data"
+        //= "If" the folder doesn't exist, 
+            // create one folder
+        //= else 
+            // "do nothing"
+        // If the script is run twice, the program overwrites the data. The file contains the data from the second call.
+    // 5) The program displays a human-friendly error (not just the original error code) 
+    //    when it cannot connect to http://shirts4mike.com
--- scraper.js
+++ scraper.js
@@ -1,25 +1,14 @@
-// ==== Prepare, Plan, Perform, Perfect ==== //
+    const http = require('http');
 
-//== Create a command line application app using Node.js For a comparaison prices sites,
-//== Should use npm moudule to assist me in project (content scraper & create csv file)
-    // At least 1,000 downloads
-    // Has been updated in the last six months
-
-//== Scrape a website once a day
+    const hostname = '127.0.0.1';
+    const port = 3000;
     
-    //== 1) uses http://shirts4mike.com/shirts.php as single entry point to scrape information for 8 tee-shirts from the site, 
-    // without using any hard-coded urls like http://www.shirts4mike.com/shirt.php?id=101
-
-    //== 2) get price, title, url and image url save this information into a CSV file name formated like: YYYY-MM-DD.csv.
-
-    //== 3) the column headers in the CSV need to be in a certain order to be correctly entered into a database. They should be in this order: Title, Price, ImageURL, URL, and Time
+    const server = http.createServer((req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello World\n');
+    });
     
-    //== 4) Save the file ine a folde called "data"
-        //= "If" the folder doesn't exist, 
-            // create one folder
-        //= else 
-            // "do nothing"
-        // If the script is run twice, the program overwrites the data. The file contains the data from the second call.
-    // 5) The program displays a human-friendly error (not just the original error code) 
-    //    when it cannot connect to http://shirts4mike.com
-
+    server.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+    });
