2f5ee60297dfcfa0dfa3a35b7c47170dd79bc4c6
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-06-08 13:16:16 +0200

    Scraping and Saving Data & Overwriting Data added to the script

--- scraper.js
+++ scraper.js
@@ -3,35 +3,39 @@ const fs = require('fs');
 const scrapeIt = require('scrape-it');
 const dateFormat = require('dateformat');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-
-let now = new Date()
-
 const hostname = '127.0.0.1';
 const port = 3000;
+let now = new Date();
 
+// Initialize server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end();
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// Read directory
+//== File streaming (make directory content and add file)
+
 fs.readdir('data', (err, files) => {
+  // If there is a file in the folder we delete it
+  if(files){
+    fs.unlink('data/'+files, (err) => {
+      if (err) throw err;
+    });
+  }
   // If directory reading is not possible, then error is thrown so... I take advantage from that to create the folder and do nothing if folder doesn't exist //
   if(err) {
-    fs.mkdir('data', () => {
-      console.log('data folder created');
-    });        
+    fs.mkdir('data', () => {});        
   }
 });
 
-// Callback interface
+//== Use http://shirts4mike.com/shirts.php as single entry point to scrape information for 8 tee-shirts from the site
 scrapeIt('http://shirts4mike.com/shirts.php', {
-    // Fetch the blog pages
+    // Fetch the blog pages and create an object of links
    links: {
         listItem: '.products li',
         name: 'pages',
@@ -42,9 +46,12 @@ scrapeIt('http://shirts4mike.com/shirts.php', {
             }
         }
     }
+    // After getting that "link's object" use data to get price, title, url and image url save this information into a CSV file name formated like: YYYY-MM-DD.csv.
 }, (err, { data }) => {
-    for (var key in data.links) {
-      let tempUrl = 'http://shirts4mike.com/'+data.links[key].url;
+    // For each data entry
+    for (let i = 0; i < data.links.length; i++) {
+      let tempUrl = 'http://shirts4mike.com/'+data.links[i].url;
+      // call scrape it then construct object (price, title, url, img, image-url)
       scrapeIt(tempUrl, {
         title: {
           selector: '.shirt-picture img',
@@ -56,11 +63,17 @@ scrapeIt('http://shirts4mike.com/shirts.php', {
         },
         price: '.price'
       }, (err, {data}) => {
+
+        //== Finalize the object with latest data
+        // Add page URL
         data.url = tempUrl;
-        data.date = dateFormat(now, 'yyyy-mm-dd');        
-        console.log(err || data);
+        // Add timestamp
+        data.date = dateFormat(now, 'yyyy-mm-dd');
+        
+        //== Then we create a csv file
         const csvWriter = createCsvWriter({
-          path: 'data/file.csv',
+          append: true,
+          path: 'data/'+data.date+'.csv',
           header: [
               {id: 'title', title: 'TITLE'},
               {id: 'imageUrl', title: 'IMAGEURL'},
@@ -70,6 +83,7 @@ scrapeIt('http://shirts4mike.com/shirts.php', {
           ]
       });
        
+      // Make the object for each line of the csv file
       const records = [
         {
           title: data.title,
@@ -79,7 +93,8 @@ scrapeIt('http://shirts4mike.com/shirts.php', {
           date: data.date         
         }
       ];
-       
+
+      // Write object record to the csv file
       Promise.resolve()
         .then(() => csvWriter.writeRecords(records))
       });
